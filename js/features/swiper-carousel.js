e74f57b177fab3d90a2a0a608463d73ab3272934
robertgraleigh <robertgraleigh@gmail.com>
2016-10-07 13:28:22 -0700

    synchronize repos

---
+++ js/features/swiper-carousel.js
@@ -0,0 +1,15 @@
+/* controls the swiper carousel feature at the top of site */
+$(document).ready(function () {
+	//initialize swiper when document ready  
+	var bannerSwiper = new Swiper ('.banner-swiper-container', {
+		// Optional parameters
+		direction: 'horizontal',
+		speed: 1000,
+		autoplay: 6000, 
+		effect: 'slide',
+		loop: true,
+		grabCursor: true,
+		paginationHide: true,
+		scrollbarHide: true
+	});        
+}(jQuery));
