5ffe1afe6318e86c5e9b430b00a05bed9a2bb38f
robertgraleigh <robertgraleigh@gmail.com>
2016-10-21 09:40:57 -0700

    sync repo

--- js/features/navigation.js
+++ js/features/navigation.js
@@ -1,7 +1,16 @@
 /* hides the collapsible dropdown menu on click */
 (function(){
-	var primaryNav = $("#navigation");
+	var primaryNav = $("#navigation-production");
 	primaryNav.on("click", "a", null, function () {
 		primaryNav.collapse("hide");
 	});
 }());
+
+function autocollapse() {
+  var navbar = $('navigation-production');
+  navbar.removeClass('collapse');
+  if(navbar.innerWidth() < 1000)
+    navbar.addClass('collapse');
+}
+jQuery(document).on('ready', autocollapse);
+jQuery(window).on('resize', autocollapse);
